36a1fea4ee94fa5bf1a86d129de78ac8d153492a
david-danie <david.danielhdz@gmail.com>
2022-01-17 12:54:13 -0600

    Se añade leer ds18b20

--- M32CTV.ino
+++ M32CTV.ino
@@ -1,178 +1,95 @@
-#include <LiquidCrystal.h>
 #include <Wire.h>
-#include <EEPROM.h>
-
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(0, 1); // RX, TX
+#include <LiquidCrystal_I2C.h>
+#include <DallasTemperature.h> 
 
 #define dsDir 0x68
+#define pcfDir 0x4E
+//
+#define bomba_o 5
+#define ventilador_o 6 
+#define luz_o 7
+#define out_3 8
+#define out_4 12
+#define out_5 13
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 24, en = 25, d4 = 26, d5 = 27, d6 = 28, d7 = 29;
+byte segundo, minuto, hora, dSem, dia, mes, ano, ctrl;
 
-byte vtime[9];
-byte segundo, minuto, hora, dSem, dia, mes, ano;
+OneWire ourWire(1);                                      // Se establece el pin 4  como bus OneWire
+DallasTemperature sensors(&ourWire); 
 
-byte v_verde, v_rojo, v_azul;
-byte fotope;
-byte dia_cre;
-byte sem_cre;
-byte pag_config0;
-bool b_config0;
-byte dato [20];
+//LiquidCrystal_I2C lcd(0x40,20,4);
+LiquidCrystal_I2C lcd(0x38, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-byte sem_ctv;
-byte dia_ctv;
-
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7); // 24,25,26,27,28,29,30
-
+// the setup function runs once when you press reset or power the board
 void setup() {
-  
+  // initialize digital pin LED_BUILTIN as an output.
+  //lcd.begin(20,4);
+  pinMode(bomba_o, OUTPUT);
+  pinMode(ventilador_o, OUTPUT);
+  pinMode(luz_o, OUTPUT);
+  pinMode(out_4, OUTPUT);
+  pinMode(out_5, OUTPUT);
   Wire.begin();
   Serial.begin(9600);
   Serial.setTimeout(1);
-  mySerial.begin(9600);
-  lcd.begin(20, 4);
-  
-  segundo = 55;              // se configura la fecha y la hora default
-  minuto = 20;              // 21 de mayo de 1936
-  hora = 4;                 // 04:20:30
-  dSem = 6;
-  dia = 21;
-  mes = 05;
-  ano = 36;
+  sensors.begin();
 
-  fotope = 0;
-  pag_config0 = 0;
-  b_config0 = 0;
 
-  write_ds3231();
+      segundo = 55;
+      minuto = 20;
+      hora = 4;
+      dia = 21;
+      mes = 5;
+      ano = 36;
   
-  while(b_config0 == 0){
-    lcd_m1(); 
-  }
+//  write_ds3231();
+//  lcd.begin(20,4);
+  
 }
 
 void loop() {
-  read_ds3231();
-  delay(1000);
-  
-  lcd_m1();
-  manda_inf();
-  
-}
 
-void manda_inf(){
-  Serial.print("D: ");
-  if (hora < 10){ Serial.print("0"); }
+  lcd.setCursor ( 0, 1 );        // go to the next line
+  lcd.print (" FORUM - fm   ");
+
+//  if (Serial.available() > 0) { 
+//    r_data(); 
+//    }
+
+  digitalWrite(bomba_o, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(ventilador_o, HIGH);
+  digitalWrite(luz_o, HIGH);
+  digitalWrite(out_4, HIGH);
+  digitalWrite(out_5, HIGH);
+  Serial.println("led encendido");
+  delay(1000);                       // wait for a second
+  digitalWrite(bomba_o, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(ventilador_o, LOW);
+  digitalWrite(luz_o, LOW);
+  digitalWrite(out_4, LOW);
+  digitalWrite(out_5, LOW);
+  //read_ds3231();
+  
+  Serial.println("led apagado");
+  Serial.print("Hora:");
   Serial.print(hora);
   Serial.print(":");
-  if (minuto < 10){ Serial.print("0"); }
   Serial.print(minuto);
   Serial.print(":");
-  if (segundo < 10){ Serial.print("0"); }
-  Serial.print(segundo);
-  Serial.print(' ');
-  if (dia < 10){ Serial.print("0"); }
-  Serial.print(dia);
-  Serial.print("/");
-  if (mes < 10){ Serial.print("0"); }
-  Serial.print(mes);
-  Serial.print("/");
-  if (ano < 10){ Serial.print("0"); }
-  Serial.println(ano);
-  
-  Serial.println(); 
+  Serial.println(segundo);
+  delay(1000);                       // wait for a second
 }
 
-void lcd_m1() {
-  lcd.setCursor(0,0); 
-  lcd.print("Nivel:");   
-  lcd.print(" ");
-
-  
-  lcd.setCursor(0,1);                                 // Se imprime la información del segundo renglon en LCD
-  lcd.print("A:");
-  lcd.print(100); 
-  lcd.print("%");
-  lcd.print(" ");
-  lcd.print("Fp:");
-  lcd.print(12);
-  lcd.print(" "); 
-  lcd.print("a:");
-  if (sem_ctv < 10){ lcd.print("0"); }
-  lcd.print(sem_ctv); 
-  lcd.print("/");
-  if (dia_ctv < 10){ lcd.print("0"); }
-  lcd.print(dia_ctv);
-
-  lcd.setCursor(0,2);                                     // Se imprime la información del tercer renglon en LCD
-  lcd.print("R:");
-  lcd.print(100); 
-  lcd.print("%");
-  lcd.print(" ");
-  lcd.setCursor(15,2);  
-  if (hora < 10){ lcd.print("0"); }                  // Se imprime la información del primer renglon en LCD
-  lcd.print(hora); lcd.print(":");
-  if (minuto < 10){ lcd.print("0"); }
-  lcd.print(minuto);
-  //lcd.print(" ");
- 
-  lcd.setCursor(0,3);                                   // Se imprime la información del cuarto renglon en LCD
-  lcd.print("V:");
-  lcd.print(100); 
-  lcd.print("%");
-  lcd.print(" ");
-  lcd.print("T:");
-  lcd.print(20);
-  lcd.print((char)223);
-  lcd.print("C"); 
-  lcd.print(" H:100%");
-}
-
-void lcd_m0() {
-  lcd.clear();
-  delay(500);
-  lcd.setCursor(4, 1);
-  lcd.print('D');
-  delay(400);
-  lcd.setCursor(3,1);
-  lcd.print('I');
-  delay(400);
-  lcd.setCursor(5,1);
-  lcd.print('A');
-  delay(400);
-  lcd.setCursor(6,1);
-  lcd.print('N');
-  delay(250);
-  lcd.setCursor(6,1);
-  lcd.print('M');
-  delay(150);
-  lcd.setCursor(7,1);
-  lcd.print('A');
-  delay(400);
-  lcd.setCursor(8,1);
-  lcd.print('R');
-  delay(400);
-  lcd.setCursor(9,1);
-  lcd.print('H');
-  delay(1000);
-  lcd.setCursor(3,1);
-  lcd.print("IDAMARH V. 1.6");
-  delay(1000);
-  lcd.clear();
-}
-
-// ==========================================================================================
-// ============================ FUNCIÓN PARA LEER EL DS3132 =================================
-// ==========================================================================================
 bool read_ds3231() {
   Wire.beginTransmission(dsDir);    // Inicia el protocolo en modo lectura.
   Wire.write(0x00);                 // Si la escritura se llevo a cabo el metodo endTransmission retorna 0
     if(Wire.endTransmission() != 0) // Terminamos la escritura y verificamos si el DS1307 respondio
       return false;                 // Escribir la dirección del segundero
   Wire.requestFrom(dsDir, 7);       // Si el DS1307 esta presente, comenzar la lectura de 8 bytes
+  
+//  for (int cont = 1; cont <= 7; cont ++){
+//    vtime[cont] = bcd2bin(Wire.read());
+//  } 
   
   segundo = bcd2bin(Wire.read());   // Recibimos el byte del registro 0x00 y lo convertimos a binario
   minuto = bcd2bin(Wire.read());    // Continuamos recibiendo cada uno de los registros
@@ -181,28 +98,30 @@ bool read_ds3231() {
   dia = bcd2bin(Wire.read());
   mes = bcd2bin(Wire.read());
   ano = bcd2bin(Wire.read());
-  //ctrl = Wire.read();  
+  ctrl = Wire.read(); 
   return true;
+  
   }                                 // Recibir los datos del registro de control en la dirección 0x07
 // ==========================================================================================
 // ============================ FUNCIÓN PARA ESCRIBIR EL DS3132 =============================
 // ==========================================================================================
 bool write_ds3231(){
   // Iniciar el intercambio de información con el DS1307 (0x68)
-  Wire.beginTransmission(dsDir);  // 1° Byte = Dirección del chip ds
+  Wire.beginTransmission(dsDir);  // 1° Byte = Dirección del chip ds1307
   Wire.write(0x00);               // 2° Byte = Dirección del registro de segundos
   Wire.write(bin2bcd(segundo)); // Escribir valores en los registros, nos aseguramos que el bit clock halt
   //Wire.write(bin2bcd(second & 0x7F));
   Wire.write(bin2bcd(minuto));        // Del registro del segundero este desactivado
   Wire.write(bin2bcd(hora));
-  Wire.write(bin2bcd(dSem));
-  //Wire.write(bin2bcd(dSem) && 0x6F);
+  Wire.write(bin2bcd(dSem) && 0x6F);
   Wire.write(bin2bcd(dia));
   Wire.write(bin2bcd(mes));
   Wire.write(bin2bcd(ano));
+  
 //  for (int cont = 1; cont <= 7; cont ++){
 //    Wire.write(bin2bcd(vtime[cont]));
 //  }
+
   if (Wire.endTransmission() != 0); // Terminamos la escritura y verificamos si el DS1307 respondio
     return false; }                 // Si la escritura se llevo a cabo el metodo endTransmission retorna 0
     
@@ -214,6 +133,138 @@ byte bcd2bin(byte bcd) {
 // =============================================================================================
 byte bin2bcd(byte bin){
   return (bin / 10 * 16) + (bin % 10); }
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DallasTemperature.h> 
+
+#define dsDir 0x68
+#define pcfDir 0x4E
+//
+#define bomba_o 5
+#define ventilador_o 6 
+#define luz_o 7
+#define out_3 8
+#define out_4 12
+#define out_5 13
+
+byte segundo, minuto, hora, dSem, dia, mes, ano, ctrl;
+
+OneWire ourWire(1);                                      // Se establece el pin 4  como bus OneWire
+DallasTemperature sensors(&ourWire); 
+
+//LiquidCrystal_I2C lcd(0x40,20,4);
+LiquidCrystal_I2C lcd(0x38, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  //lcd.begin(20,4);
+  pinMode(bomba_o, OUTPUT);
+  pinMode(ventilador_o, OUTPUT);
+  pinMode(luz_o, OUTPUT);
+  pinMode(out_4, OUTPUT);
+  pinMode(out_5, OUTPUT);
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.setTimeout(1);
+  sensors.begin();
+
+
+      segundo = 55;
+      minuto = 20;
+      hora = 4;
+      dia = 21;
+      mes = 5;
+      ano = 36;
+  
+//  write_ds3231();
+//  lcd.begin(20,4);
+  
+}
+
+void loop() {
+
+  lcd.setCursor ( 0, 1 );        // go to the next line
+  lcd.print (" FORUM - fm   ");
+
+//  if (Serial.available() > 0) { 
+//    r_data(); 
+//    }
+
+  digitalWrite(bomba_o, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(ventilador_o, HIGH);
+  digitalWrite(luz_o, HIGH);
+  digitalWrite(out_4, HIGH);
+  digitalWrite(out_5, HIGH);
+  Serial.println("led encendido");
+  delay(1000);                       // wait for a second
+  digitalWrite(bomba_o, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(ventilador_o, LOW);
+  digitalWrite(luz_o, LOW);
+  digitalWrite(out_4, LOW);
+  digitalWrite(out_5, LOW);
+  //read_ds3231();
+  
+  Serial.println("led apagado");
+  Serial.print("Hora:");
+  Serial.print(hora);
+  Serial.print(":");
+  Serial.print(minuto);
+  Serial.print(":");
+  Serial.println(segundo);
+  delay(1000);                       // wait for a second
+}
+
+bool read_ds3231() {
+  Wire.beginTransmission(dsDir);    // Inicia el protocolo en modo lectura.
+  Wire.write(0x00);                 // Si la escritura se llevo a cabo el metodo endTransmission retorna 0
+    if(Wire.endTransmission() != 0) // Terminamos la escritura y verificamos si el DS1307 respondio
+      return false;                 // Escribir la dirección del segundero
+  Wire.requestFrom(dsDir, 7);       // Si el DS1307 esta presente, comenzar la lectura de 8 bytes
+  
+//  for (int cont = 1; cont <= 7; cont ++){
+//    vtime[cont] = bcd2bin(Wire.read());
+//  } 
+  
+  segundo = bcd2bin(Wire.read());   // Recibimos el byte del registro 0x00 y lo convertimos a binario
+  minuto = bcd2bin(Wire.read());    // Continuamos recibiendo cada uno de los registros
+  hora = bcd2bin(Wire.read());
+  dSem = bcd2bin(Wire.read());
+  dia = bcd2bin(Wire.read());
+  mes = bcd2bin(Wire.read());
+  ano = bcd2bin(Wire.read());
+  ctrl = Wire.read(); 
+  return true;
+  
+  }                                 // Recibir los datos del registro de control en la dirección 0x07
 // ==========================================================================================
-// ====================== Imprime la fecha por puerto serial ==========================
+// ============================ FUNCIÓN PARA ESCRIBIR EL DS3132 =============================
 // ==========================================================================================
+bool write_ds3231(){
+  // Iniciar el intercambio de información con el DS1307 (0x68)
+  Wire.beginTransmission(dsDir);  // 1° Byte = Dirección del chip ds1307
+  Wire.write(0x00);               // 2° Byte = Dirección del registro de segundos
+  Wire.write(bin2bcd(segundo)); // Escribir valores en los registros, nos aseguramos que el bit clock halt
+  //Wire.write(bin2bcd(second & 0x7F));
+  Wire.write(bin2bcd(minuto));        // Del registro del segundero este desactivado
+  Wire.write(bin2bcd(hora));
+  Wire.write(bin2bcd(dSem) && 0x6F);
+  Wire.write(bin2bcd(dia));
+  Wire.write(bin2bcd(mes));
+  Wire.write(bin2bcd(ano));
+  
+//  for (int cont = 1; cont <= 7; cont ++){
+//    Wire.write(bin2bcd(vtime[cont]));
+//  }
+
+  if (Wire.endTransmission() != 0); // Terminamos la escritura y verificamos si el DS1307 respondio
+    return false; }                 // Si la escritura se llevo a cabo el metodo endTransmission retorna 0
+    
+// ==========================================================================================
+// ====================== Conversiones bcd a binario/binario a bcd ==========================
+// ==========================================================================================
+byte bcd2bin(byte bcd) {
+  return (bcd / 16 * 10) + (bcd % 16); } // Convertir decenas y luego unidades a un numero binario
+// =============================================================================================
+byte bin2bcd(byte bin){
+  return (bin / 10 * 16) + (bin % 10); }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # M32CTV
 Los invernaderos ofrecen muchas ventajas sobre los métodos de agricultura tradicionales y el cultivo de algunas hortalizas y otras plantas en las ciudades cada día es más común. La tecnología actual en los invernaderos puede incluir la automatización de tareas y control de parámetros; medición/control de humedad, temperatura, PH, Automatización de riego, ventilación, fotoperiodo, etc.
 <br>
-Los dispositivos y la comunidad opensource son de ayuda para integrar esta tecnología en sistemas dedicados <img src="https://camo.githubusercontent.com/c55beef2f138da61fe671a1e4a307ff4ffbc318d/68747470733a2f2f692e696d6775722e636f6d2f715849456368542e6a7067" alt="imagen" width="500" height="240"/><br>
+Los dispositivos y la comunidad opensource son de ayuda para integrar esta tecnología en sistemas dedicados <img src="./atmegaPinout.png" alt="imagen" width="300" height="300"/><br>
 
 ## DESCRIPCIÓN
 ## TARJETA DE CONTROL
